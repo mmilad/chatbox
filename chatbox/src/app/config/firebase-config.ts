7d73fc036adeddabb3482390c17beddaf26bd8f4
milad <m.ashrafi@compuccino.com>
2017-07-10 15:38:17 +0200

    removed alled

--- chatbox/src/app/config/firebase-config.ts
+++
@@ -1,8 +0,0 @@
-export const FirebaseConfig = {
-    apiKey: "AIzaSyDWENhgpkRN3OcKjliL-SEhrq21Gn7vix4",
-    authDomain: "community-1d2a2.firebaseapp.com",
-    databaseURL: "https://community-1d2a2.firebaseio.com",
-    projectId: "community-1d2a2",
-    storageBucket: "community-1d2a2.appspot.com",
-    messagingSenderId: "479800108779"
-};
